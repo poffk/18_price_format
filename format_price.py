41e00ad9eb0875ccdb711fe3e9092b97eb4b81ce
Alexander <Alexander>
2017-01-22 13:34:22 +0300

    Первая отправка

--- format_price.py
+++ format_price.py
@@ -1,5 +1,36 @@
+import argparse
+from re import search
+
+
+def parse_price():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price',
+                        help='введите значение(цену) для форматирования')
+    return parser.parse_args()
+
+
+def make_price_beatiful(price):
+    beatiful_price = '{0:,.2f}'.format(price).replace(',', ' ')
+    if search(r'\d*[.]00', beatiful_price):
+        beatiful_price = beatiful_price[:-3]
+    return beatiful_price
+
+
 def format_price(price):
-    pass
+    try:
+        price = float(price)
+    except ValueError:
+        return None
+    if price < 0:
+        return None
+    return make_price_beatiful(price)
+
 
 if __name__ == '__main__':
-    pass
+    parser = parse_price()
+    price = parser.price
+    formatted_price = format_price(price)
+    if formatted_price is None:
+        print('Ошибка ввода')
+    else:
+        print('Отформатированная цена: {}'.format(formatted_price))
--- tests.py
+++ tests.py
@@ -1,1 +1,28 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestFormattingPrice(unittest.TestCase):
+    def test_int(self):
+        price = format_price(4645)
+        self.assertEqual(price, '4 645')
+
+    def test_float(self):
+        price = format_price(4645.0)
+        self.assertEqual(price, '4 645')
+
+    def test_string(self):
+        price = format_price('4645.00')
+        self.assertEqual(price, '4 645')
+
+    def test_incorrect_input(self):
+        formatted_price = format_price('4645abc')
+        self.assertEqual(formatted_price, None)
+
+    def test_negative_number(self):
+        formatted_price = format_price(-4645)
+        self.assertEqual(formatted_price, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
